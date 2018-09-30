69e46194b4c59862ab821c0ed439fb58ef475cbf
dgarrimar <dgarrimar@gmail.com>
2018-09-30 21:18:35 +0200

    select top k variants

--- uvic/AdvBI_2018/data/hands-on/eQTL/bin/compZscore.R
+++ uvic/AdvBI_2018/data/hands-on/eQTL/bin/compZscore.R
@@ -13,6 +13,9 @@ suppressPackageStartupMessages(library(dplyr))
 option_list = list(
   make_option(c("-g", "--gene"), type = "character",
               help = "Gene name", metavar = "character"),
+  make_option(c("-k", "--k"), type = "numeric",
+              help = "Number of top variants to consider [default %default]", 
+              default = 100, metavar = "numeric"),
   make_option(c("-n", "--nominals"), type = "character",
               help = "Output of QTLtools nominal pass", metavar = "character"),
   make_option(c("-o", "--output"), type="character",
@@ -32,6 +35,8 @@ set.seed(123)
 tb <- as.data.frame(fread(opt$nominal, header = T))[,c(1,8,12,13)]
 colnames(tb) <- c("pheno_id", "var_id", "pv", "slope")
 tb <- subset(tb, pheno_id == opt$gene) 
+tb <- tb[order(tb$pv), ]
+tb <- tb[1:opt$k,]
 
 tb$Z <- qnorm(tb$pv, lower.tail = FALSE)*sign(tb$slope)
 tb <- subset(tb, select = c("var_id", "Z"))
